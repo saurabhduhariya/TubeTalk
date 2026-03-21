7c81916a7b4663a50ddc102044a5859f1b476857
sauduhariya <saurabhduhariya2007@gmail.com>
2026-03-21 17:20:38 +0530

    add side panel support and background script for YouTube chatbot

---
+++ frontend/public/background.js
@@ -0,0 +1,9 @@
+// background.js
+
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
+
+chrome.action.onClicked.addListener((tab) => {
+  // This helps ensure side panel opens for specific site/action
+});
